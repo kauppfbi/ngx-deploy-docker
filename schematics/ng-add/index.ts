caa6b6c798235b053917fc87391925f32cf6d129
Fabian K <fabian.kaupp@mail.de>
2019-12-30 20:55:49 +0100

    fix: update ng-add schematic
    use 'account' instead of 'userName' in ng-add schematic

--- schematics/ng-add/index.ts
+++ schematics/ng-add/index.ts
@@ -96,7 +96,7 @@ function addDeployBuilderToProject(tree: Tree, options: NgAddOptions) {
   project.architect['deploy'] = {
     builder: 'ngx-deploy-docker:deploy',
     options: {
-      userName: options.userName
+      account: options.account
     }
   };
 
